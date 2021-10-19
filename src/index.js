5ca786c7453e595def9bed38a7f4219de6528d78
Bruno Kohn <brunokohn1993@gmail.com>
2021-10-19 15:14:02 -0300

    importando atraves do components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./styles/reset.css";
-import Login from "./Login.js";
-import Register from "./Register.js";
-import Records from "./Records.js";
-import NewIncome from "./NewIncome.js";
-import NewExpense from "./NewExpense.js";
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import Records from "./components/Records.js";
+import NewIncome from "./components/NewIncome.js";
+import NewExpense from "./components/NewExpense.js";
 
 function App() {
   return (
