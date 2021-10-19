63e759225728a58040841dd017a8e2daca669a5c
Bruno Kohn <brunokohn1993@gmail.com>
2021-10-19 09:03:18 -0300

    arquivos iniciais

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <h1>Hello, World</h1>
+    );
+}
+
+const app = App();
+ReactDOM.render(app, document.querySelector(".root"));
